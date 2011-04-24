9a4b4a04ca86a6889f6301080f3faef06459434b
Will Crawford <will@metawhimsy.com>
2011-04-24 15:32:36 -0700

    Made sbi.scm a little more portable across our dev platforms, and added some comments.

--- 2/sbi.scm
+++ 2/sbi.scm
@@ -1,5 +1,10 @@
-#!/afs/cats.ucsc.edu/courses/cmps112-wm/usr/racket-5.1/bin/mzscheme -qr
-;; $Id: sbi.scm,v 1.1 2011-04-08 20:17:51-07 - - $
+#!/usr/bin/env mzscheme
+#lang scheme
+;#!/afs/cats.ucsc.edu/courses/cmps112-wm/usr/racket-5.1/bin/mzscheme -qr
+;; AUTHORS
+;;    Will Crawford <wacrawfo@ucsc.edu>
+;;    Ben Ross      <bpross@ucsc.edu>
+;;    Based on code by Wesley Mackey
 ;;
 ;; NAME
 ;;    sbi.scm - silly basic interpreter
@@ -12,49 +17,52 @@
 ;;    SBIR program, which is the executed.  Currently it is only
 ;;    printed.
 ;;
-
+; Define STDERR
 (define *stderr* (current-error-port))
-
+; What is this?
 (define *run-file*
     (let-values
         (((dirpath basepath root?)
             (split-path (find-system-path 'run-file))))
         (path->string basepath))
 )
-
+; What is this?
 (define (die list)
     (for-each (lambda (item) (display item *stderr*)) list)
     (newline *stderr*)
     (exit 1)
 )
-
+; Usage.
 (define (usage-exit)
     (die `("Usage: " ,*run-file* " filename"))
 )
-
+; What is this?
 (define (readlist-from-inputfile filename)
     (let ((inputfile (open-input-file filename)))
          (if (not (input-port? inputfile))
              (die `(,*run-file* ": " ,filename ": open failed"))
              (let ((program (read inputfile)))
                   (close-input-port inputfile)
-                         program))))
-
+                         program)))
+)
+; What is this?
 (define (write-program-by-line filename program)
     (printf "==================================================~n")
     (printf "~a: ~s~n" *run-file* filename)
     (printf "==================================================~n")
     (printf "(~n")
     (map (lambda (line) (printf "~s~n" line)) program)
-    (printf ")~n"))
-
+    (printf ")~n")
+)
+; This is the main function that gets called.
 (define (main arglist)
     (if (or (null? arglist) (not (null? (cdr arglist))))
         (usage-exit)
         (let* ((sbprogfile (car arglist))
                (program (readlist-from-inputfile sbprogfile)))
-              (write-program-by-line sbprogfile program))))
-
+              (write-program-by-line sbprogfile program)))
+)
+; Execute the main function.
 (main (vector->list (current-command-line-arguments)))
 
 
