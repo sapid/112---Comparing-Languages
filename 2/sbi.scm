4499ebb8a2bc24af636c94b3e9c5f44e5ec72386
Will Crawford <will@metawhimsy.com>
2011-04-24 16:41:35 -0700

    Commented the sample code thoroughly.

--- 2/sbi.scm
+++ 2/sbi.scm
@@ -2,65 +2,70 @@
 #lang scheme
 ;#!/afs/cats.ucsc.edu/courses/cmps112-wm/usr/racket-5.1/bin/mzscheme -qr
 ;; AUTHORS
-;;    Will Crawford <wacrawfo@ucsc.edu>
-;;    Ben Ross      <bpross@ucsc.edu>
-;;    Based on code by Wesley Mackey
+;;   Will Crawford <wacrawfo@ucsc.edu>
+;;   Ben Ross     <bpross@ucsc.edu>
+;;   Based on code by Wesley Mackey
 ;;
 ;; NAME
-;;    sbi.scm - silly basic interpreter
+;;   sbi.scm - silly basic interpreter
 ;;
 ;; SYNOPSIS
-;;    sbi.scm filename.sbir
+;;   sbi.scm filename.sbir
 ;;
 ;; DESCRIPTION
-;;    The file mentioned in argv[1] is read and assumed to be an
-;;    SBIR program, which is the executed.  Currently it is only
-;;    printed.
+;;   The file mentioned in argv[1] is read and assumed to be an
+;;   SBIR program, which is the executed.  Currently it is only
+;;   printed.
 ;;
-; Define STDERR
+; Define *stderr*
 (define *stderr* (current-error-port))
-; What is this?
+; Function: Find the basename of the filename provided.
 (define *run-file*
-    (let-values
-        (((dirpath basepath root?)
-            (split-path (find-system-path 'run-file))))
-        (path->string basepath))
+   (let-values
+      (((dirpath basepath root?)
+         (split-path (find-system-path 'run-file))))
+      (path->string basepath))
 )
-; What is this?
+; Function: Exit and print the list provided.
 (define (die list)
-    (for-each (lambda (item) (display item *stderr*)) list)
-    (newline *stderr*)
-    (exit 1)
+   (for-each (lambda (item) (display item *stderr*)) list)
+   (newline *stderr*)
+   (exit 1)
 )
-; Usage.
+; Function: Print usage information and die.
 (define (usage-exit)
-    (die `("Usage: " ,*run-file* " filename"))
+   (die `("Usage: " ,*run-file* " filename"))
 )
-; What is this?
+; Function: Read in the file.
 (define (readlist-from-inputfile filename)
-    (let ((inputfile (open-input-file filename)))
-         (if (not (input-port? inputfile))
-             (die `(,*run-file* ": " ,filename ": open failed"))
-             (let ((program (read inputfile)))
-                  (close-input-port inputfile)
-                         program)))
+   (let ((inputfile (open-input-file filename)))
+       (if (not (input-port? inputfile))
+          (die `(,*run-file* ": " ,filename ": open failed"))
+          (let ((program (read inputfile)))
+              (close-input-port inputfile)
+                   program)))
 )
 ; What is this?
 (define (write-program-by-line filename program)
-    (printf "==================================================~n")
-    (printf "~a: ~s~n" *run-file* filename)
-    (printf "==================================================~n")
-    (printf "(~n")
-    (map (lambda (line) (printf "~s~n" line)) program)
-    (printf ")~n")
+   (printf "==================================================~n")
+   (printf "~a: ~s~n" *run-file* filename)
+   (printf "==================================================~n")
+   (printf "(~n")
+   ; For each list element in program, print each element.
+   (map (lambda (line) (printf "~s~n" line)) program)
+   (printf ")~n")
 )
 ; This is the main function that gets called.
 (define (main arglist)
-    (if (or (null? arglist) (not (null? (cdr arglist))))
-        (usage-exit)
-        (let* ((sbprogfile (car arglist))
-               (program (readlist-from-inputfile sbprogfile)))
-              (write-program-by-line sbprogfile program)))
+   (if (or (null? arglist) (not (null? (cdr arglist))))
+      ; Case 1. Number of args != 1.
+      (usage-exit) 
+      ; Case 2. Set sbprogfile = filename from argument
+      (let* ((sbprogfile (car arglist))
+            ; Set program = The list of commands in the inputfile.
+            (program (readlist-from-inputfile sbprogfile))) 
+      ; Call write-program-by-line(sbprogfile, program)
+      (write-program-by-line sbprogfile program)))
 )
 ; Execute the main function.
 (main (vector->list (current-command-line-arguments)))
