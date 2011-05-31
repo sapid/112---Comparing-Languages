9ea7870d531b8858f050db0bbcf41401115f3f28
Will Crawford <will@metawhimsy.com>
2011-05-31 11:17:22 -0700

    Added authorship notes and removed garbage code.

--- 5/functions.pl
+++ 5/functions.pl
@@ -1,7 +1,7 @@
-/*
-* Function: Distance between airports.
-* Tested using SWI prolog.
-*/
+% Authors
+%     Will Crawford <wacrawfo@ucsc.edu>
+%     Ben Ross      <bpross@ucsc.edu>
+%     Significant contributions from Wesley Mackey
 
 not(X) :- X, !, fail.
 not(_).
@@ -102,25 +102,6 @@ listpath( Node, End, Tried, [Node|List] ) :-
    not( member( Next, Tried )),
    listpath( Next, End, [Next|Tried], List ).
 
-
-
-% 
-
-%connected(X,Y) :- flight(X, Y, _).
-%
-%flight_path( A, B, Path) :- 
-%   travel(A,B,[A],Q), 
-%   reverse(Q,Path).
-%
-%travel(A,B,P,[B|P]) :- 
-%   connected(A,B).
-%
-%travel(A,B,Visited,Path) :-
-%   connected(A,C),           
-%   C \== B,
-%   \+member(C,Visited),
-%   travel(C,B,[C|Visited],Path).  
-   
 fly( Depart, Arrive ) :-
    listpath(Depart, Arrive, List),
    writepath(List).
